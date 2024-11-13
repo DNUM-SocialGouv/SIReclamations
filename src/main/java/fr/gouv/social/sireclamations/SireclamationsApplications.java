1e5adcb3a74d61fb479a7ca196753a8f953f7369
OCTO-BEZA <benoit.bazart@octo.com>
2024-11-13 18:24:01 +0100

    Ajout de Logstash Logback Encoder pour log au format JSON (sortie vers la Console)

--- src/main/java/fr/gouv/social/sireclamations/SireclamationsApplications.java
+++ src/main/java/fr/gouv/social/sireclamations/SireclamationsApplications.java
@@ -1,5 +1,7 @@
 package fr.gouv.social.sireclamations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SireclamationsApplications {
 
+	private static final Logger logger = LoggerFactory.getLogger(SireclamationsApplications.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SireclamationsApplications.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		logger.info("Bienvenue dans la fonction hello!");
 		return String.format("Hello %s!", name);
 	}
 }
