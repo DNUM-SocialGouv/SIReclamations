2807d40a0d89e3cfe0539c238c4b2d69481a987c
anthony-teixeira-octo <anthony.teixeira@octo.com>
2024-11-13 15:34:07 +0100

    projet SI reclamations premier commit

---
+++ src/test/java/fr/gouv/social/sireclamations/SireclamationsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gouv.social.sireclamations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SireclamationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
