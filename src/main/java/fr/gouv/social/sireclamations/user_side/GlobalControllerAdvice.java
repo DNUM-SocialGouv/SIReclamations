20669849ea629be1e7b52a48c60098c2ba2d9cb5
Benoit Bazart <benoit.bazart@octo.com>
2025-03-09 18:29:51 +0100

    simal-70: (HACK) DS webhook

--- src/main/java/fr/gouv/social/sireclamations/user_side/GlobalControllerAdvice.java
+++ src/main/java/fr/gouv/social/sireclamations/user_side/GlobalControllerAdvice.java
@@ -4,19 +4,23 @@ import fr.gouv.social.sireclamations.hexagone.exceptions.AutoriteCompetenteNotFo
 import fr.gouv.social.sireclamations.hexagone.exceptions.CodePostalAbsentException;
 import fr.gouv.social.sireclamations.hexagone.exceptions.ContactNotFoundException;
 import fr.gouv.social.sireclamations.hexagone.exceptions.DematSocialException;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
   private static final Logger logger = LoggerFactory.getLogger(GlobalControllerAdvice.class);
+  public static final String STATUS = "status";
+  public static final String MESSAGE = "message";
 
   @ExceptionHandler(AutoriteCompetenteNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -24,7 +28,7 @@ public class GlobalControllerAdvice {
   public Map<String, String> handleAutoriteCompetenteNotFoundException(
       AutoriteCompetenteNotFoundException ex) {
     logger.error(ex.getMessage());
-    return Map.of("error", ex.getMessage());
+    return getGlobalControllerAdviceBodyResponse(HttpStatus.NOT_FOUND, ex.getMessage());
   }
 
   @ExceptionHandler(ContactNotFoundException.class)
@@ -32,7 +36,7 @@ public class GlobalControllerAdvice {
   @ResponseBody
   public Map<String, String> handleContactNotFoundException(ContactNotFoundException ex) {
     logger.error(ex.getMessage());
-    return Map.of("error", ex.getMessage());
+    return getGlobalControllerAdviceBodyResponse(HttpStatus.NOT_FOUND, ex.getMessage());
   }
 
   @ExceptionHandler(DematSocialException.class)
@@ -40,7 +44,7 @@ public class GlobalControllerAdvice {
   @ResponseBody
   public Map<String, String> handleDematSocialException(DematSocialException ex) {
     logger.error(ex.getMessage());
-    return Map.of("error", ex.getMessage());
+    return getGlobalControllerAdviceBodyResponse(HttpStatus.NOT_FOUND, ex.getMessage());
   }
 
   @ExceptionHandler(CodePostalAbsentException.class)
@@ -48,7 +52,7 @@ public class GlobalControllerAdvice {
   @ResponseBody
   public Map<String, String> handleCodePostalAbsentException(CodePostalAbsentException ex) {
     logger.error(ex.getMessage());
-    return Map.of("error", ex.getMessage());
+    return getGlobalControllerAdviceBodyResponse(HttpStatus.NOT_FOUND, ex.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
@@ -56,6 +60,13 @@ public class GlobalControllerAdvice {
   @ResponseBody
   public Map<String, String> handleGenericException(Exception ex) {
     logger.error(ex.getMessage());
-    return Map.of("error", "Une erreur inattendue s'est produite." + ex.getMessage());
+    return getGlobalControllerAdviceBodyResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+  }
+
+  public static Map<String,String> getGlobalControllerAdviceBodyResponse(HttpStatus httpStatus, String message) {
+    return Map.of(STATUS, String.valueOf(httpStatus.value()), MESSAGE, message);
+  }
+  public static ResponseEntity<Map<String, String>> getGlobalControllerAdviceResponse(HttpStatus httpStatus, String message) {
+    return ResponseEntity.status(httpStatus).body(getGlobalControllerAdviceBodyResponse(httpStatus, message));
   }
 }
--- src/main/java/fr/gouv/social/sireclamations/user_side/ReclamationController.java
+++ src/main/java/fr/gouv/social/sireclamations/user_side/ReclamationController.java
@@ -1,29 +1,110 @@
 package fr.gouv.social.sireclamations.user_side;
 
+import fr.gouv.social.sireclamations.hexagone.DeposerReclamation;
+import fr.gouv.social.sireclamations.hexagone.domain.Reclamation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/reclamations")
 @Tag(name = "Réclamations", description = "Endpoints pour gérer les réclamations")
 public class ReclamationController {
   private static final Logger logger = LoggerFactory.getLogger(ReclamationController.class);
+  private static final String NUMERO_DEMARCHE_KEY = "procedure_id";
+  private static final String NUMERO_DOSSIER_KEY = "dossier_id";
+  private static final String ETAT_KEY = "state";
+  private static final String DATE_DEPOT_KEY = "updated_at";
+  private static final String DATETIME_PATTERN_KEY = "yyyy-MM-dd HH:mm:ss Z";
 
+  private final DeposerReclamation deposerReclamation;
+
+  public ReclamationController(DeposerReclamation deposerReclamation) {
+    this.deposerReclamation = deposerReclamation;
+  }
+
+  //hack: une nouvelle version du webhook doit prochainement être intégrée. Cette version enverra une payload de type application/json. Cette méthode est créée dans l'attente.
   @PostMapping(consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-  public ResponseEntity<String> handleNonBrowserSubmissions(
-      @RequestParam MultiValueMap<String, String> paramMap) {
+  @Operation(
+          summary = "Récupère une réclamation issue de demat.social",
+          description = "Permet de récupérer une réclamation issue de demat.social grâce un numéro de dossier transmis. Cette API est un webhook destiné à être configuré dans demat.social",
+          responses = {
+                  @ApiResponse(responseCode = "200", description = "Réclamation affectée avec succès"),
+                  @ApiResponse(responseCode = "400", description = "Données invalides dans la requête"),
+                  @ApiResponse(responseCode = "404", description = "Ressources nécessaires introuvables"),
+                  @ApiResponse(responseCode = "500", description = "Erreur interne du serveur")
+          })
+  public ResponseEntity<Map<String, String>> handleNonBrowserSubmissions(
+      @RequestParam Map<String, String> dsWebhookMap) {
 
-    logger.info("MultiValueMap : {}", paramMap.toString());
+    logger.info("Appel Webhook reçu avec les paramètres : {}", dsWebhookMap);
 
-    return new ResponseEntity<String>(paramMap.toString(), HttpStatus.OK);
+    String message = "";
+
+    if(isValidPayload(dsWebhookMap)) {
+      final DeposerReclamationRequest.Etat etat = DeposerReclamationRequest.Etat.valueOf(dsWebhookMap.get(ETAT_KEY).toUpperCase());
+      final int numeroProcedure = Integer.parseInt(dsWebhookMap.get(NUMERO_DEMARCHE_KEY));
+      final int numeroDossier = Integer.parseInt(dsWebhookMap.get(NUMERO_DOSSIER_KEY));
+      final LocalDateTime dateDepot = LocalDateTime.parse(dsWebhookMap.get(DATE_DEPOT_KEY), DateTimeFormatter.ofPattern(DATETIME_PATTERN_KEY));
+      final DeposerReclamationRequest deposerReclamationRequest = new DeposerReclamationRequest(numeroProcedure, numeroDossier, etat, dateDepot);
+
+      if(deposerReclamationRequest.getEtat() == DeposerReclamationRequest.Etat.EN_CONSTRUCTION) {
+        final Reclamation reclamation = deposerReclamation.executer(deposerReclamationRequest.getNumeroDossier());
+        message = ReclamationApiMapper.toReclamationApiResponse(reclamation).toString();
+        logger.info("La réclamation : {} à été affectée.", message);
+        return GlobalControllerAdvice.getGlobalControllerAdviceResponse(HttpStatus.OK, message);
+      } else {
+        message = String.format("Le Dossier %s présenté n'est pas à l'état de Construction.", deposerReclamationRequest.getNumeroDossier());
+        logger.error(message);
+        return GlobalControllerAdvice.getGlobalControllerAdviceResponse(HttpStatus.BAD_REQUEST, message);
+      }
+    } else {
+      message = "Paramètres envoyés invalides.";
+      logger.error(message);
+      return GlobalControllerAdvice.getGlobalControllerAdviceResponse(HttpStatus.BAD_REQUEST, message);
+    }
+  }
+
+  private static boolean validateInteger(Map<String, String> params, String key) {
+    try {
+      Integer.parseInt(params.get(key));
+      return true;
+    } catch (NumberFormatException e) {
+      return false;
+    }
+  }
+
+  private static boolean validateState(Map<String, String> params, String key) {
+    return DeposerReclamationRequest.Etat.isValid(params.get(key));
+  }
+
+  private static boolean validateDate(Map<String, String> params, String key) {
+    try {
+      LocalDateTime.parse(params.get(key), DateTimeFormatter.ofPattern(DATETIME_PATTERN_KEY));
+      return true;
+    } catch (DateTimeParseException e) {
+      return false;
+    }
+  }
+
+  private static boolean isValidPayload(Map<String, String> params) {
+    return params.size() == 4 && validateInteger(params, NUMERO_DEMARCHE_KEY)
+            && validateInteger(params, NUMERO_DOSSIER_KEY)
+            && validateState(params, ETAT_KEY)
+            && validateDate(params, DATE_DEPOT_KEY);
   }
 }
