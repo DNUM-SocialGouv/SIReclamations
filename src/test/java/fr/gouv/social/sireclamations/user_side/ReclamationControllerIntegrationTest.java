88791da44e55c6ee8db9e353e764bc9efed0d8e6
Benoit Bazart <benoit.bazart@octo.com>
2025-03-10 15:39:22 +0100

    simal-70: spotless:apply

--- src/test/java/fr/gouv/social/sireclamations/user_side/ReclamationControllerIntegrationTest.java
+++ src/test/java/fr/gouv/social/sireclamations/user_side/ReclamationControllerIntegrationTest.java
@@ -5,7 +5,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
